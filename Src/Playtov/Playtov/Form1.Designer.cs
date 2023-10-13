a2a724c75422f2e5d87829f5cc9ae647efad8a3a
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-13 03:19:14 +0200

    Remove white bckg at start

--- Src/Playtov/Playtov/Form1.Designer.cs
+++ Src/Playtov/Playtov/Form1.Designer.cs
@@ -43,6 +43,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1342, 721);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
